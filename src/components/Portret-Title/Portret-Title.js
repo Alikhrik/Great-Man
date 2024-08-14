7e3ecebf893d9fd5e3b7009dbbb7c0d8a19cdfb3
Alikhrik <siderdt@gmail.com>
2024-08-15 00:10:09 +0300

    Added class version of components

--- src/components/Portret-Title/Portret-Title.js
+++ src/components/Portret-Title/Portret-Title.js
@@ -1,13 +1,26 @@
 import React from "react";
 import styles from './Portret-Title.module.css';
 
-export function PortretTitle(props) {
-    return (
-    <dib>
-        <div className={styles['full-name']}>Гринцевич Назарій Андрійович</div>
-        <div>
-            <span className={styles.date}>10 березня 2003</span> - <span className={styles.date}>6 травня 2024</span>
-        </div>
-    </dib>
-    );
+// export function PortretTitle(props) {
+//     return (
+//     <dib>
+//         <div className={styles['full-name']}>Гринцевич Назарій Андрійович</div>
+//         <div>
+//             <span className={styles.date}>10 березня 2003</span> - <span className={styles.date}>6 травня 2024</span>
+//         </div>
+//     </dib>
+//     );
+// }
+
+export class PortretTitle extends React.Component {
+    render() {
+        return (
+            <dib>
+                <div className={styles['full-name']}>Гринцевич Назарій Андрійович</div>
+                <div>
+                    <span className={styles.date}>10 березня 2003</span> - <span className={styles.date}>6 травня 2024</span>
+                </div>
+            </dib>
+            );
+    }
 }
--- src/components/Portret/Portret.js
+++ src/components/Portret/Portret.js
@@ -3,9 +3,18 @@ import { PortretTitle } from "../Portret-Title/Portret-Title";
 import photoImage from '../../assets/images/photo.jpeg'; 
 import styles from './Portret.module.css';
 
-export function Portret(props) {
-    return (<div className={styles.centred}>
-        <img src={photoImage} className={styles.photo} alt="" />
-        <PortretTitle></PortretTitle>
-    </div>);
+// export function Portret(props) {
+//     return (<div className={styles.centred}>
+//         <img src={photoImage} className={styles.photo} alt="" />
+//         <PortretTitle></PortretTitle>
+//     </div>);
+// }
+
+export class Portret extends React.Component {
+    render() {
+        return (<div className={styles.centred}>
+            <img src={photoImage} className={styles.photo} alt="" />
+            <PortretTitle></PortretTitle>
+        </div>);
+    }
 }
