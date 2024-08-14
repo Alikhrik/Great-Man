12ded069f4118635b7d0c4dcda6af047dc466371
Alikhrik <siderdt@gmail.com>
2024-08-15 00:03:23 +0300

    Task 2 Completed

---
+++ src/components/Portret-Title/Portret-Title.js
@@ -0,0 +1,13 @@
+import React from "react";
+import styles from './Portret-Title.module.css';
+
+export function PortretTitle(props) {
+    return (
+    <dib>
+        <div className={styles['full-name']}>Гринцевич Назарій Андрійович</div>
+        <div>
+            <span className={styles.date}>10 березня 2003</span> - <span className={styles.date}>6 травня 2024</span>
+        </div>
+    </dib>
+    );
+}
---
+++ src/components/Portret/Portret.js
@@ -0,0 +1,11 @@
+import React from "react";
+import { PortretTitle } from "../Portret-Title/Portret-Title";
+import photoImage from '../../assets/images/photo.jpeg'; 
+import styles from './Portret.module.css';
+
+export function Portret(props) {
+    return (<div className={styles.centred}>
+        <img src={photoImage} className={styles.photo} alt="" />
+        <PortretTitle></PortretTitle>
+    </div>);
+}
